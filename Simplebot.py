a4e3321a0e8e466ab02065426beffa0b9cddb32c
Ilgiz khafizov <@ii.hafizov@yandex.ru>
2021-04-27 21:40:37 +0300

    Simple bot 1.0

---
+++ Simplebot.py
@@ -0,0 +1,33 @@
+import logging
+import settings
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+	level=logging.INFO,
+	filename='bot.log'
+	)
+
+def start_bot(update: Updater, CallbackContext):
+	mytext = """Привет, {}!
+Я пока не очень умный бот и знаю только команду /start. Можешь нажать еще раз :)""".format(update.message.chat.first_name)
+	update.message.reply_text(mytext)
+	print("Hello")
+
+def chat(update: Updater, CallbackContext):
+	text = "Создатель еще не научил меня понимать слова. Я понимаю только команды, и то пока что одну: /start"
+	logging.info(text)
+
+	update.message.reply_text(text)
+
+def main():
+	updtr = Updater(settings.TOKEN_TELEGRAM)
+
+	updtr.dispatcher.add_handler(CommandHandler("start", start_bot))
+	updtr.dispatcher.add_handler(MessageHandler(Filters.text, chat))
+
+	updtr.start_polling()
+	updtr.idle()
+
+if __name__ == "__main__":
+	logging.info('Bot started')
+	main()
